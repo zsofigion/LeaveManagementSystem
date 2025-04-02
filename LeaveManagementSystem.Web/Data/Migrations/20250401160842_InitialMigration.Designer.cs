cbf591859448ae275f56cc034d24c2fa7757c695
zsofigion <zsofi.gion@gmail.com>
2025-04-02 19:22:37 +0200

    Add database and first migrations

--- LeaveManagementSystem.Web/Data/Migrations/20250401160842_InitialMigration.Designer.cs
+++ LeaveManagementSystem.Web/Data/Migrations/20250401160842_InitialMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LeaveManagementSystem.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeaveManagementSystem.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250401160842_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.3");
